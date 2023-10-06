4383e0f7a4316f21229bd0e8317a20aa8e3192e5
abdullah <abdullahomrana12312332110h@gmail.com>
2023-10-06 20:40:41 +0300

    updated

--- src/app/home/patient-encounter/demographics/page.tsx
+++ src/app/home/patient-encounter/demographics/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 import styles from "./page.module.scss";
 import { useDispatch } from "react-redux";
-import { setActiveSideMenuItem,setActiveSubNavItem } from "@/redux/doctorSlice";
+import {
+  setActiveSideMenuItem,
+  setActiveSubNavItem,
+} from "@/redux/doctorSlice";
+import { Form, InputGroup } from "react-bootstrap";
 
 function Demographics() {
-    const dispatch = useDispatch();
-    dispatch(setActiveSideMenuItem(1));
-    dispatch(setActiveSubNavItem(0));
+  const dispatch = useDispatch();
+  dispatch(setActiveSideMenuItem(1));
+  dispatch(setActiveSubNavItem(0));
   return (
     <div className={styles.container}>
-      0
+       <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="inputPassword5"
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Text id="passwordHelpBlock" muted>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </Form.Text>
     </div>
   );
 }
