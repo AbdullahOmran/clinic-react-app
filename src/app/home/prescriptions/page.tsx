2a86f812a9b7d59edea140772bee9aa95b21e8cd
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-02-04 11:40:00 +0200

    updated

--- src/app/home/prescriptions/page.tsx
+++ src/app/home/prescriptions/page.tsx
@@ -5,7 +5,7 @@ import {
   setActiveSideMenuItem,
   setActiveTreatmentPlansSubNavItem,
 } from "@/redux/doctorSlice";
-import { Button, Card, CloseButton, Col, Container, Row, Stack } from "react-bootstrap";
+import { Button, Card, CloseButton, Col, Container, ListGroup, Row, Stack } from "react-bootstrap";
 import { BsCheckCircle, BsPlusCircle, BsXCircle } from "react-icons/bs";
 import { CiViewList } from "react-icons/ci";
 import { useState } from "react";
@@ -34,28 +34,11 @@ function Prescriptions() {
                 variant="primary"
               >
                 <BsPlusCircle className={styles.icon} />
-                Add Medication
+                Generate Prescription
                 <span className="visually-hidden">unread messages</span>
               </Button>
               
-              <Button
-              
-                className="p-1"
-                variant="primary"
-              >
-                <BsPlusCircle className={styles.icon} />
-                Add New Medication
-                <span className="visually-hidden">unread messages</span>
-              </Button>
-              <Button
-                
-                className="p-1"
-                variant="primary"
-              >
-                <BsCheckCircle className={styles.icon} />
-                Drug Interaction checker
-                <span className="visually-hidden">unread messages</span>
-              </Button>
+             
             </Stack>
           </Row>
           <Row className="mb-1">
@@ -64,8 +47,85 @@ function Prescriptions() {
               Prescriptions List
             </h3>
           </Row>
-          <Row className="g-4">
-            
+          <Row className="g-4 mb-2">
+          <ListGroup as="ol" numbered>
+              <ListGroup.Item
+                as="li"
+                action
+                className={clsx({
+                  "d-flex justify-content-between align-items-center ": true
+                })}
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Prescription1</div>
+                  Date
+                </div>
+                <Button className="p-1" variant="primary">
+                Show
+                  <span className="visually-hidden">unread messages</span>
+                </Button>
+                <CloseButton className="ms-3" />
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                action
+                className={clsx({
+                  "d-flex justify-content-between align-items-center ": true,
+                  
+                })}
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Prescription2</div>
+                  Date
+                </div>
+                <Button className="p-1" variant="primary">
+                Show
+                  <span className="visually-hidden">unread messages</span>
+                </Button>
+                <CloseButton className="ms-3" />
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                action
+                className={clsx({
+                  "d-flex justify-content-between align-items-center ": true,
+                  
+                })}
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Prescription3</div>
+                  Date
+                </div>
+                <Button className="p-1" variant="primary">
+                Show
+                  <span className="visually-hidden">unread messages</span>
+                </Button>
+                <CloseButton className="ms-3" />
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                action
+                className={clsx({
+                  "d-flex justify-content-between align-items-center ": true,
+                  
+                })}
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Prescription4</div>
+                  Date
+                </div>
+                <Button  className="p-1" variant="primary">
+                  Show
+                  <span className="visually-hidden">unread messages</span>
+                </Button>
+                <CloseButton className="ms-3" />
+              </ListGroup.Item>
+            </ListGroup>
+          </Row>
+          <Row className="mb-1">
+                <div className={styles.viewer}>
+                  Select an item to show
+                </div>
           </Row>
         </Container>
       </div>
