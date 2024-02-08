7505f175a1efd3da29b0ab1f527f201d87d43713
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-02-08 14:22:09 +0200

    updated

--- src/utils/authLogic.ts
+++ src/utils/authLogic.ts
@@ -21,3 +21,25 @@ export const loginUser = async (e: any) => {
     return null;
   }
 };
+export const updateTokens = async (tokens: any)=>{
+  try {
+    
+    const res = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refresh: tokens.refresh,
+      }),
+    });
+    const data = await res.json();
+    if (res.status === 200) {
+      return data;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    return null;
+  }
+}
