c6aa47101ec0c4f78aa193096d9b305600ddc887
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-04-23 10:05:31 +0200

    updated

--- src/utils/authLogic.ts
+++ src/utils/authLogic.ts
@@ -24,4 +24,26 @@ export const loginUser = async (e: any) => {
   }
 };
 
+export const registerUser = async(data: any)=>{
+  try {
+  const res = await fetch("http://127.0.0.1:8000/api/auth/register/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    
+    if (res.status === 201) {
+      return data;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    return null;
+  }
+};
+
+  
+
   
