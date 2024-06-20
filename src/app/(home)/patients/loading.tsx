bade12b5d13345b58f9380bf600f6ffa69d3362c
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-06-20 16:15:31 +0300

    updated

---
+++ src/app/(home)/patients/loading.tsx
@@ -0,0 +1,7 @@
+import Loader from "@/components/Loader";
+function Loading(){
+    return <div style={{width:"100%",height:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}>
+        <Loader />
+    </div>;
+}
+export default Loading ;
